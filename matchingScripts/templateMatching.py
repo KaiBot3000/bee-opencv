8ab6ca9613badc4819ccdd7912f5f946fc7c16cc
Kai <riselikethemoon@gmail.com>
2018-02-26 16:27:04 -0500

    get multiple matches using threshold

--- matchingScripts/templateMatching.py
+++ matchingScripts/templateMatching.py
@@ -23,6 +23,9 @@ comb_color = cv.imread(os.path.join(dir_name, '../img/combBee.png'))
 single_gray = cv.cvtColor(single_color, cv.COLOR_BGR2GRAY)
 comb_gray = cv.cvtColor(comb_color, cv.COLOR_BGR2GRAY)
 
+# Height and width of template for drawing rectangles later
+h, w = single_color.shape[:-1]
+
 # flip channels- countouring works on black background, not white
 single_flip = cv.bitwise_not(single_gray)
 
@@ -33,31 +36,43 @@ ret, mask = cv.threshold(single_flip, 60, 255, 0)
 ### Match against template and find best match
 
 result = cv.matchTemplate(comb_gray, single_gray, cv.TM_SQDIFF, mask)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-top_left = min_loc
+# result = cv.matchTemplate(comb_gray, single_gray, cv.TM_CCORR_NORMED)
 
-### Show results
+### Single best match
+# min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+# print 'min:', min_val, ' max:', max_val
+# top_left = min_loc
 
 # Draw rectangle using match + height and width of template
-h, w = single_color.shape[:-1]
-bottom_right = (top_left[0] + w, top_left[1] + h)
-cv.rectangle(comb_color, top_left, bottom_right, 255, 2)
+# bottom_right = (top_left[0] + w, top_left[1] + h)
+# cv.rectangle(comb_color, top_left, bottom_right, 255, 2)
+
+### Multiple matches
+threshold = 80000000 # min: 79277216.0, max: 146592384.0
+matches = np.where(result <= threshold)
+print len(matches[0])
+# unzip matches into points 
+for match in zip(*matches[::-1]):
+    bottom_right = (match[0] + w, match[1] + h)
+    cv.rectangle(comb_color, match, bottom_right, 255, 2)
+
+### Show results
 
 # add some headspace between plots
 plt.subplots_adjust(hspace=0.5)
 
 # row, column, order (weird)
-plt.subplot(411), plt.imshow(single_gray,cmap='gray')
-plt.title('Template'), plt.xticks([]), plt.yticks([])
+# plt.subplot(411), plt.imshow(single_gray,cmap='gray')
+# plt.title('Template'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(412), plt.imshow(mask,cmap='gray')
-plt.title('Mask'), plt.xticks([]), plt.yticks([])
+# plt.subplot(412), plt.imshow(mask,cmap='gray')
+# plt.title('Mask'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(413), plt.imshow(result,cmap='gray')
-plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+# plt.subplot(413), plt.imshow(result,cmap='gray')
+# plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
 
 # plt and cv store images in reverse color: rgb v. bgr, so convert:
-plt.subplot(414), plt.imshow(cv.cvtColor(comb_color, cv.COLOR_BGR2RGB))
+plt.subplot(111), plt.imshow(cv.cvtColor(comb_color, cv.COLOR_BGR2RGB))
 plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
 
 plt.show()

154d5b4d8367cf54d341aaa135a6869d6e64605d
Kai <riselikethemoon@gmail.com>
2018-02-27 12:40:36 -0500

    tidy

--- matchingScripts/keypointDetection.py
+++ matchingScripts/keypointDetection.py
@@ -15,9 +15,6 @@ comb_bee_color = cv.imread(os.path.join(dir_name, '../img/singleCombBee.png'))
 # convert to grayscale
 single_bee_gray = cv.cvtColor(single_bee_color, cv.COLOR_BGR2GRAY)
 comb_bee_gray = cv.cvtColor(comb_bee_color, cv.COLOR_BGR2GRAY)
-# cv2.imshow('comb_bee_gray', comb_bee_gray)
-# cv2.waitKey(1000) # wait for 1s before continuing program
-# cv2.destroyAllWindows() # destroy, although this seems to happen anyhow
 
 # sift detector
 orb = cv.ORB_create()
@@ -25,11 +22,6 @@ orb = cv.ORB_create()
 # find keypoints, descriptors using sift detector
 keypoint_single, descriptor_single = orb.detectAndCompute(single_bee_gray, None)
 keypoint_comb, descriptor_comb = orb.detectAndCompute(comb_bee_gray, None)
-
-# draw and show keypoints on images
-# kp_single_bee = cv.drawKeypoints(single_bee_gray, keypoint_single, None, color=(0,255,0), flags=0)
-# kp_comb_bee = cv.drawKeypoints(comb_bee_gray, keypoint_comb, None, color=(0,255,0), flags=0)
-# plt.imshow(kp_comb_bee), plt.show()
 
 # orb match, then sort by distance
 matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
